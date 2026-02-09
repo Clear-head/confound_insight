7efe83707e2d57a7048a3d00123b86a5f9adf75b
saseoghun <huni032285@gmail.com>
2026-02-09 17:48:11 +0900

    [refactor] to analysis

--- api/compounds/fingerprint.py
+++ api/compounds/fingerprint.py
@@ -2,7 +2,7 @@ from rdkit import Chem
 from rdkit.Chem import rdMolDescriptors, DataStructs
 from django.db.backends.utils import logger
 
-def generate_morgan_binary(smiles: str, radius: int = 2, n_bits: int = 2048) -> bytes | None:
+async def generate_morgan_binary(smiles: str, radius: int = 2, n_bits: int = 2048) -> bytes | None:
     """
     SMILES -> Morgan Fingerprint -> Binary(bytes) 변환
     """
@@ -26,14 +26,3 @@ def generate_morgan_binary(smiles: str, radius: int = 2, n_bits: int = 2048) -> 
     except Exception as e:
         logger.error(f"Fingerprint 생성 중 오류 발생: {e}")
         return None
-
-def get_similarity(bin_fp1: bytes, bin_fp2: bytes) -> float:
-    """
-    저장된 두 이진 지문 간의 Tanimoto 유사도 계산 (0.0 ~ 1.0)
-    """
-    from rdkit.DataStructs import cDataStructs
-
-    fp1 = cDataStructs.CreateFromBinaryText(bin_fp1.decode('latin-1'))
-    fp2 = cDataStructs.CreateFromBinaryText(bin_fp2.decode('latin-1'))
-
-    return DataStructs.TanimotoSimilarity(fp1, fp2)
