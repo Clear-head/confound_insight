c6bb562420a082ec505ef9e45d1435a5a36ce0b6
saseoghun <huni032285@gmail.com>
2026-02-10 16:14:22 +0900

    [refactor] mfds api success

--- api/products/mfds.py
+++ api/products/mfds.py
@@ -1,15 +1,15 @@
 from httpx import AsyncClient
 from django.conf import settings
+from django.db.backends.utils import logger
 
-
-async def search_product_by_name(search_name):
+async def search_product_by_name(search_name: str) -> dict:
     url = "https://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService07/getDrugPrdtMcpnDtlInq07"
 
-    all_items = []
+    result = {}
     page_no = 1
-    num_of_rows = 30
+    num_of_rows = 100
 
-    async with AsyncClient() as client:
+    async with AsyncClient(timeout=10.0) as client:
         while True:
             params = {
                 "serviceKey": settings.MFDS_API_KEY,
@@ -22,20 +22,34 @@ async def search_product_by_name(search_name):
             try:
                 response = await client.get(url, params=params)
                 response.raise_for_status()
-                data = response.json()
+                res_data = response.json()
 
-                body = data.get("body", {})
-                items = body.get("items", [])
-                total_count = body.get("totalCount", 0)
+                items = res_data.get("body", {}).get("items", [])
+                total_count = res_data.get("body", {}).get("totalCount", 0)
 
-                all_items.extend(items)
 
-                if len(all_items) >= total_count:
+                if not items:
                     break
 
+                for item in items:
+                    item_seq = item.get("ITEM_SEQ")
+                    if result.get(item_seq, None) is None:
+                        result[item_seq] = {
+                            "product_name": item.get("PRDUCT"),
+                            "manufacturer": item.get("ENTRPS"),
+                            "permit_number": item.get("MTRAL_CODE"),
+                            "ingredients_en": list(map(str, item.get("MAIN_INGR_ENG").split("/"))),
+                            "is_combination": False if len(item.get("MAIN_INGR_ENG").split("/")) == 1 else True,
+                        }
+                    else:
+                        pass
+
+                if len(items) < num_of_rows or (page_no * num_of_rows) >= total_count:
+                    break
                 page_no += 1
 
             except Exception as e:
-                raise e
+                logger.error(f"식약처 API 데이터 처리 중 오류: {e}")
+                break
 
-    return all_items
+    return result
